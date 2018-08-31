de7581ac7b019c06d5eb374adffc32d8d57449e7
syzer <syzer@users.noreply.github.com>
2018-08-31 15:25:45 +0200

    Fix: Fix fails for some Id's (#4)
    fixes #3 
    
    * Add tests, and test runner
    
    * Add failing test
    
    * Add flowconfig
    
    * Fix #3: Fails for `en` and videoId `JueUvj6X3DA`

---
+++ test/index.test.js
@@ -0,0 +1,16 @@
+import test from 'ava';
+import { getSubtitles } from '../src';
+
+test('Extract estonia war subtitles', async t => {
+  const subtitles = await getSubtitles({ videoID: 'HBA0xDHZjko' });
+  t.deepEqual(subtitles[0], {
+    dur: '5.87',
+    start: '6.62',
+    text: 'November 19',
+  });
+});
+
+test('Extract passive income video', async t => {
+  const subtitles = await getSubtitles({ videoID: 'JueUvj6X3DA' });
+  t.deepEqual(subtitles[0].text, '- Creating passive income takes work,');
+});
