4a834a88ae38ab37f3b30fe04bd3325910d2ba54
unknown <tombyrer@gmail.com>
2020-04-26 20:37:48 -0500

    v2: new output & cli, dropped lodash

--- README.md
+++ README.md
@@ -4,20 +4,20 @@
 
 ## Installation
 
-* `> npm install -S youtube-captions-scraper` OR
-* `> yarn add youtube-captions-scraper`
+* `npm install -S youtube-captions-scraper` OR
+* `yarn add youtube-captions-scraper`
 
 ## Usage
 
 ```js
 // ES6 / TypeScript
-import { getSubtitles } from 'youtube-captions-scraper';
+import { getSubtitles } from 'youtube-captions-scraper'
 
 getSubtitles({
   videoID: 'XXXXX', // youtube video id
   lang: 'fr' // default: `en`
 }).then(captions => {
-  console.log(captions);
+  console.log(captions)
 });
 
 // ES5
@@ -31,12 +31,33 @@ getSubtitles({
 });
 ```
 
-Captions will be an array of object of this format:
+### Output
+
+**v2 change**: Captions will be an array of object of this format:
 
 ```js
-{
-  "start": Number,
-  "dur": Number,
-  "text": String
-}
+"videoID": "9W0Dy1nM-zU",
+"lang": "en",
+"datePull": 1587948820739,
+"timedtext": [
+  {
+    "s": 11.5,
+    "d": 4,
+    "t": "<b>Bold</b>"
+  },
+etc...
 ```
+Key:
+ * videoID = YouTube's unique id after `v=` in URL querry string
+ * lang = language code of captions
+ * datePull = [seconds stamp](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/now) when the captions was pulled & processed
+ * timedtext array = **s**tart time, **d**uration, & **t**ext
+
+
+### CLI
+
+```bash
+node cli VIDEO-ID
+```
+Outputs in home dir as `VIDEO-ID.captions.json`.  
+The `.gitignore` prevents you from saving the output in your git repo.
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 
 import he from 'he';
 import axios from 'axios';  // includes JSON transforms & XSRF protection
-import { find } from 'lodash';
 
 export async function getSubtitles({
   videoID,
@@ -14,33 +13,27 @@ export async function getSubtitles({
   const { data } = await axios.get(
     `https://youtube.com/get_video_info?video_id=${videoID}`
   );
-
-  const decodedData = decodeURIComponent(data);
+  const decodedData = decodeURIComponent(data)
 
   // * ensure we have access to captions data
   if (!decodedData.includes('captionTracks'))
-    throw new Error(`Could not find captions for video: ${videoID}`);
+    throw new Error(`Could not find captions for video: ${videoID}`)
 
-  const regex = /({"captionTracks":.*isTranslatable":(true|false)}])/;
-  const [match] = regex.exec(decodedData);
-  const { captionTracks } = JSON.parse(`${match}}`);
-
-  const subtitle =
-    find(captionTracks, {
-      vssId: `.${lang}`,
-    }) ||
-    find(captionTracks, {
-      vssId: `a.${lang}`,
-    }) ||
-    find(captionTracks, ({ vssId }) => vssId && vssId.match(`.${lang}`));
+  const regex = /({"captionTracks":.*isTranslatable":(true|false)}])/
+  const [match] = regex.exec(decodedData)
+  const { captionTracks } = JSON.parse(`${match}}`)
+  const subtitle = captionTracks.find( ({ vssId }) => vssId && vssId.match(`.${lang}`) )
 
   // * ensure we have found the correct subtitle lang
   if (!subtitle || (subtitle && !subtitle.baseUrl))
-    throw new Error(`Could not find ${lang} captions for ${videoID}`);
+    throw new Error(`Could not find ${lang} captions for ${videoID}`)
 
-  const { data: transcript } = await axios.get(subtitle.baseUrl);
+  const { data: transcript } = await axios.get(subtitle.baseUrl)
 
-  const lines = transcript
+  let timedtext = []
+  const startRegex = /start="([\d.]+)"/;
+  const durRegex = /dur="([\d.]+)"/
+  transcript
     .replace('<?xml version="1.0" encoding="utf-8" ?><transcript>', '')
     .replace(/&lt;font color=&quot;#......&quot;&gt;/gi, '')
     .replace(/&lt;\/font&gt;/gi, '')
@@ -48,22 +41,21 @@ export async function getSubtitles({
     .replace('</transcript>', '')
     .split('</text>')
     .filter(line => line && line.trim())
-    .map(line => {
-      const startRegex = /start="([\d.]+)"/;
-      const durRegex = /dur="([\d.]+)"/;
-      const [, start] = startRegex.exec(line);
-      const [, dur] = durRegex.exec(line);
-  
-      const htmlText = line
-        .replace(/<text.+>/, '');
-      const text = he.decode(htmlText);
+    .map( (line, i) =>{
+      let cue = {}
+      const [, start] = startRegex.exec(line)
+      cue.s = parseFloat(start)
+      const [, dur] = durRegex.exec(line)
+      cue.d = parseFloat(dur)
+      const htmlText = line.replace(/<text.+>/, '')
+      cue.t = he.decode(htmlText)
+      timedtext[i] = cue
+    })
 
-      return {
-        start,
-        dur,
-        text,
-      };
-    });
-
-  return lines;
+  return {
+    videoID,
+    lang,
+    datePull:  Date.now(),
+    timedtext,
+  }
 }
